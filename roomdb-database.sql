d890a309dfc4011992831176daac1ad16ad0ab13
[dilshani] <[dilshanithathsara@gmail.com]>
2020-11-07 20:37:16 +0530

    database added

--- roomdb-database.sql
+++ roomdb-database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 13, 2020 at 04:25 PM
+-- Generation Time: Nov 03, 2020 at 05:59 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -52,7 +52,8 @@ CREATE TABLE `room_discount` (
 INSERT INTO `room_discount` (`discount_id`, `discount_rate`, `start_date`, `end_date`) VALUES
 (1, 0, '2020-02-20', '2020-05-06'),
 (2, 0, '2020-02-20', '2020-05-21'),
-(3, 0, '2020-10-16', '0000-00-00');
+(3, 0, '2020-10-16', '0000-00-00'),
+(4, 0, '2020-10-27', '2020-10-29');
 
 -- --------------------------------------------------------
 
@@ -75,9 +76,10 @@ CREATE TABLE `room_type` (
 --
 
 INSERT INTO `room_type` (`type_id`, `type_name`, `floor_no`, `room_price`, `max_guest`, `facilities`, `description`) VALUES
-(2, '3d', 1, 7000, 3, 'AC', 'fully AC '),
-(3, 'seaside', 1, 900, 2, 'AC', 'none'),
-(4, '6t', 2, 5000, 2, 'nnn', 'jjjj');
+(2, '3d', 1, 6000, 3, 'AC', 'non Ac '),
+(3, 'seaside', 1, 10000, 2, 'AC', 'none'),
+(4, '6t', 2, 5000, 2, 'nnn', 'jjjj'),
+(5, 'dd', 2, 3500, 5, 'AC', 'none');
 
 --
 -- Indexes for dumped tables
@@ -115,13 +117,13 @@ ALTER TABLE `room`
 -- AUTO_INCREMENT for table `room_discount`
 --
 ALTER TABLE `room_discount`
-  MODIFY `discount_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `discount_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `room_type`
 --
 ALTER TABLE `room_type`
-  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
